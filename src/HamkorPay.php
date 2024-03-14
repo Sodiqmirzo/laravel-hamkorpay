9deff19bc27197fb341cac23f225441eda1ad2f9
Sodiqmirzo Sattorov <mr.sodiqmirzosattorov17@gmail.com>
2024-03-14 15:33:11 +0500

    auth bug fixed

--- src/HamkorPay.php
+++ src/HamkorPay.php
@@ -48,7 +48,7 @@ class HamkorPay
         $username = $this->config['username'];
         $password = $this->config['password'];
         $request = $this->client->baseUrl($tokenUrl)
-            ->withToken(base64_encode($username . ':' . $password))
+            ->withHeaders(['Authorization' => 'Basic ' . base64_encode($username . ':' . $password)])
             ->post('/oauth2/token', ['grant_type' => 'client_credentials']);
 
         throw_if($request['access_token'] === null, new HamkorPayTokenNotFound('HamkorPay token not found', -1025));
