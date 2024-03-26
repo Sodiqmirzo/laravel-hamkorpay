54881078c3aa1aef900e9b2427ddea9b489ebb9d
Sodiqmirzo Sattorov <mr.sodiqmirzosattorov17@gmail.com>
2024-03-26 13:56:38 +0500

    added additional fields card response

--- src/Responses/Card/CardResponse.php
+++ src/Responses/Card/CardResponse.php
@@ -11,13 +11,17 @@ class CardResponse extends BaseResponse
     public function __construct(
         public string $number,
         public string $expiry,
-        public int    $balance
+        public int    $balance,
+        public string $owner,
+        public string $phone,
+        public string $status,
+        public bool   $is_sms_enabled,
     )
     {
     }
 
     public function isOk(): bool
     {
-        return $this->number !== null;
+        return (int)$this->status === 0;
     }
 }
